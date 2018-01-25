1182a7fa7607ee7ef64eebec39dee4ffe02a5e01
Cedric Rutland Wienold <cwienold@gmail.com>
2018-01-25 22:09:58 +0900

    Apply middleware/plugins to restify

--- src/infrastructure/server/index.ts
+++ src/infrastructure/server/index.ts
@@ -2,10 +2,34 @@ import * as restify from 'restify';
 
 require('dotenv').config();
 
+const prePlugins = [
+    restify.plugins.pre.context(),
+    restify.plugins.pre.sanitizePath(),
+    restify.plugins.pre.userAgentConnection()
+];
+
+const plugins = [
+    restify.plugins.acceptParser([
+        'application/json'
+    ]),
+    restify.plugins.authorizationParser(),
+    restify.plugins.queryParser(),
+    restify.plugins.bodyParser(),
+    restify.plugins.requestLogger(),
+    restify.plugins.gzipResponse(),
+];
+
 const server = restify.createServer();
+
+prePlugins.map(plugin => server.pre(plugin));
+plugins.map(plugin => server.use(plugin));
+
 server.get('/', (request, response, next) => {
     response.send('hello, world!');
     next();
 });
 
-server.listen(process.env.PORT, () => console.log('%s listening at %s', server.name, server.url));
+server.listen(
+    process.env.PORT,
+    () => console.log('%s listening at %s', server.name, server.url)
+);
