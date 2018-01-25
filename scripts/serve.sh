9f8db363d7cbc3dbe9fc7bff3dfb32f2bc0aa4b2
Cedric Rutland Wienold <cwienold@gmail.com>
2018-01-25 22:09:58 +0900

    Add env-based serve script

---
+++ scripts/serve.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Load environment variables from .env file
+export $(egrep -v '^#' .env | xargs)
+
+case ${NODE_ENV} in
+  production)
+    node dist/index.js
+    ;;
+  *)
+    ts-node src/infrastructure/server/index.ts
+    ;;
+esac
