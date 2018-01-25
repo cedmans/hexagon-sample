ec1341ac71edaeb67815e2a361ab546e97628569
Cedric Rutland Wienold <cwienold@gmail.com>
2018-01-25 22:09:58 +0900

    Add PORT to dotenv, use for serving

--- src/infrastructure/server/index.ts
+++ src/infrastructure/server/index.ts
@@ -1,9 +1,11 @@
 import * as restify from 'restify';
 
+require('dotenv').config();
+
 const server = restify.createServer();
 server.get('/', (request, response, next) => {
     response.send('hello, world!');
     next();
 });
 
-server.listen(8080, () => console.log('%s listening at %s', server.name, server.url));
+server.listen(process.env.PORT, () => console.log('%s listening at %s', server.name, server.url));
